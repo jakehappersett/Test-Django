f4298a867062b8e9f4a8587ed7b115816c05c537
jakehappersett <jake.happersett@gmail.com>
2018-05-12 10:38:26 -0400

    finished part 2, admin site is configured

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,33 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Question(models.Model):
+    '''
+    Question set for poll app
+    '''
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    '''
+    Choice set for polls app
+    '''
+    def __str__(self):
+        return self.choice_text
+
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
